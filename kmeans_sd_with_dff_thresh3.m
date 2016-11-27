a20dc5e5a23a5dea98b000dd85285daaafee3ba1
Patrick <patrickzhang@berkeley.edu>
2016-11-27 00:18:01 -0800

    removed extra files

--- kmeans_sd_with_dff_thresh3.m
+++ kmeans_sd_with_dff_thresh3.m
@@ -1,4 +1,4 @@
-function [clusters , spikes_cluster_idx , baseline_cluster_idx , rasterSpikeTime , dff1 , dff_snr] = kmeans_sd_with_dff_thresh2(trace_features , ROI_tracein , K , dff_thresh)
+function [clusters , spikes_cluster_idx , baseline_cluster_idx , rasterSpikeTime , dff1 , dff_snr] = kmeans_sd_with_dff_thresh3(trace_features , ROI_tracein , K , dff_thresh)
     % use polynomial regression to flatten the trace and calc df/f
     ROI_trace = traceFlattener(ROI_tracein , 1);
 %     baseline = median(ROI_trace);
