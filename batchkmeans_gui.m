786f545a6627aa13b3b1ac849f2ae7fa71295da6
Patrick Zhang <patrickpzzhang@gmail.com>
2018-01-03 16:34:36 -0800

    Add integration for PCA/ICA ROI selection

--- batchkmeans_gui.m
+++ batchkmeans_gui.m
@@ -70,12 +70,14 @@ if baseDir == 0
     return
 end
 handles.listenForTKey = 0;
-set(handles.folder_text , 'String' , baseDir)
-roiFilePaths = recursdir(baseDir , '^roi.*\.mat$');
+set(handles.folder_text , 'String' , baseDir);
 WINDOW = 50;
 K = 3;
 REARM_FACTOR = 2;
 
+% For each roi-*.mat file, generate traces and cluster. Saves results to
+% spikes-*.mat file in the same directory.
+roiFilePaths = recursdir(baseDir , '^roi.*\.mat$');
 for iRoiFile = 1:numel(roiFilePaths)
     load(roiFilePaths{iRoiFile});
     [roiPath,roiName,roiExt] = fileparts(roiFilePaths{iRoiFile});
@@ -113,9 +115,62 @@ for iRoiFile = 1:numel(roiFilePaths)
          'roiTraces','diffFeatures','snapPath', 'textPos', 'roiMasks');
         backgroundOption = selectedString;
         save(saveDir,'backgroundOption','-append');
-        
     end
 end
+
+% For each PCA/ICA file, which already has background adjusted traces, just
+% cluster and save to a spikes-*.mat file in the same directory. PCA/ICA
+% files are assumed to be of the format ica_traces_maps_*.mat.
+% Frame rate assumed to be 500 fps.
+icaStart = 'ica_traces_maps_';
+frameRate = 500;
+
+icaFilePaths = recursdir(baseDir , ['^' icaStart '.*\.mat$']);
+for iIcaFile = 1:numel(icaFilePaths)
+    load(icaFilePaths{iIcaFile});
+    [icaPath,icaName,icaExt] = fileparts(icaFilePaths{iIcaFile});
+    
+    msg = sprintf('Processing PCA/ICA file %d of %d...',...
+        iIcaFile,numel(icaFilePaths));
+    set(handles.info_txt , 'String' , msg);
+    
+    % Extract traces from PCA/ICA analysis.
+    for iTrace = 1:size(traces_cells, 2)
+        trace = traces_cells(:, iTrace)';
+        
+        % WORKAROUND for negative baseline
+        if min(trace) < 0
+            trace = trace - min(trace);
+        end
+        
+        bkgSubtractedTraces{iTrace} = trace;
+        roiTraces{iTrace} = trace;
+    end
+
+    % K-means clustering
+    nTrace = numel(bkgSubtractedTraces);
+    diffFeatures = cell(1,nTrace);
+    spikeDataArray = cell(1,nTrace);
+
+    for iTrace = 1:nTrace
+        diffFeatures{iTrace} = slidingwindowflattener(...
+            bkgSubtractedTraces{iTrace} , WINDOW);
+        spikeDataArray{iTrace} = spikekmeans(diffFeatures{iTrace},...
+            bkgSubtractedTraces{iTrace}, K);
+        spikeDataArray{iTrace}.rasterSpikeTimes = burstaggregator(...
+            spikeDataArray{iTrace}.rasterSpikeTimes, REARM_FACTOR);
+    end
+    
+    % Get tag at the end of the ica file name
+    tag = icaName(numel(icaStart)+1:end);
+    saveDir = [icaPath '/spikes-' tag '.mat'];
+    disp(['Saving data to ' saveDir])
+    save(saveDir,'spikeDataArray','frameRate','bkgSubtractedTraces' ,...
+     'roiTraces','diffFeatures');
+    backgroundOption = 'ica/pca';
+    save(saveDir,'backgroundOption','-append');
+end
+
 set(handles.info_txt , 'String' , 'Done! Press ''T'' to proceed to thresholding or select another folder for processing.');
 handles.listenForTKey = 1;
 
